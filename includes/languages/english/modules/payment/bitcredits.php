d0deade711cc25a015a1b2c099adc8c07f71875a
Alexandre Bourget <alex@bourget.cc>
2014-03-17 16:59:08 -0400

    Removed .io and cap for Bitcredits

--- includes/languages/english/modules/payment/bitcredits.php
+++ includes/languages/english/modules/payment/bitcredits.php
@@ -1,10 +1,10 @@
 <?php
 
 //	Bitcredits Payment Module - Lanugage File
-define('MODULE_PAYMENT_BITCREDITS_TEXT_TITLE', 'Bitcoin via bitcredits.io');
-define('MODULE_PAYMENT_BITCREDITS_TEXT_DESCRIPTION', 'Use bitcredits.io\'s payment processing server to automatically accept bitcoins.');
-define('MODULE_PAYMENT_BITCREDITS_TEXT_EMAIL_FOOTER', 'You just paid with bitcoins via bitcredits.io -- Thanks!');
+define('MODULE_PAYMENT_BITCREDITS_TEXT_TITLE', 'Bitcoin via Bitcredits');
+define('MODULE_PAYMENT_BITCREDITS_TEXT_DESCRIPTION', 'Use Bitcredits\'s payment processing server to automatically accept bitcoins.');
+define('MODULE_PAYMENT_BITCREDITS_TEXT_EMAIL_FOOTER', 'You just paid with Bitcoin via Bitcredits -- Thanks!');
 
 // Error messages:
-define('MODULE_PAYMENT_BITCREDITS_BAD_CURRENCY', 'Currency not supported by bitcredits.io.  Please choose another currency.');
-define('MODULE_PAYMENT_BITCREDITS_CREATE_INVOICE_FAILED', 'Unable to process payment using bitcredits.  Please choose another form of payment.');
+define('MODULE_PAYMENT_BITCREDITS_BAD_CURRENCY', 'Currency not supported by Bitcredits.  Please choose another currency.');
+define('MODULE_PAYMENT_BITCREDITS_CREATE_INVOICE_FAILED', 'Unable to process payment using Bitcredits.  Please choose another form of payment.');
